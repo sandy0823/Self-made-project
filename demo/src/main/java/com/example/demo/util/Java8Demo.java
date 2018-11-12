dd6f853ea3de901891228d4dd86bc36fc06ab043
sandy0823 <sandyflower@yeah.net>
2018-11-12 22:10:04 +0800

    [ADD]添加tomcat 8.0.53源码

---
+++ demo/src/main/java/com/example/demo/util/Java8Demo.java
@@ -0,0 +1,7 @@
+package com.example.demo.util;
+
+import java.util.stream.IntStream;
+
+public class Java8Demo {
+   
+}
---
+++ demo/src/main/java/com/example/demo/util/StreamJava8Demo.java
@@ -0,0 +1,34 @@
+package com.example.demo.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamJava8Demo {
+   public static void main(String[] args){
+	   List<Person> list = Arrays.asList(new Person("fd"),new Person("tt"),new Person("rr"));
+	   list.stream()
+               .map(Person::getName)
+               .collect(Collectors.toList()).forEach(System.out::println);
+	   
+	   IntStream.rang
+   }
+   
+   public static class Person{
+	   
+	   public Person(String name){
+		   this.name =name;
+	   }
+	   private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}   
+   }
+}
